22d1a4a3d898788e81374147484fa9a9efcb38b6
mohammadRaiyyan <inforaiyyan@gmail.com>
2025-09-25 23:10:10 +0530

    Practiced useDebounce hook, detect product in view manully

---
+++ html-js/detect-in-viewport/main.js
@@ -0,0 +1,41 @@
+(function () {
+    function debounce(fn, delay) {
+        var timerId;
+        return function () {
+            var _this = this;
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            clearTimeout(timerId);
+            timerId = setTimeout(function () { return fn.apply(_this, args); }, delay);
+        };
+    }
+    function createBlocks() {
+        var wrapper = document.querySelector(".wrapper");
+        var blocks = Array.from({ length: 64 }, function (_, i) {
+            var block = document.createElement("div");
+            block.className = "block";
+            block.textContent = "".concat(i);
+            return block;
+        });
+        wrapper.append.apply(wrapper, blocks);
+    }
+    createBlocks();
+    function isInViewPort(block) {
+        var rect = block === null || block === void 0 ? void 0 : block.getBoundingClientRect();
+        return (rect === null || rect === void 0 ? void 0 : rect.top) >= 0 && (rect === null || rect === void 0 ? void 0 : rect.left) >= 0 && rect.bottom <= document.documentElement.clientHeight && rect.right <= document.documentElement.clientWidth;
+    }
+    function detect() {
+        var results = [];
+        document.querySelectorAll(".block")
+            .forEach(function (block) {
+            if (isInViewPort(block)) {
+                results.push(block.textContent || "");
+            }
+        });
+        console.log("Items In View: ", results);
+    }
+    var debounceScroll = debounce(detect, 500);
+    window.addEventListener("scroll", debounceScroll, false);
+})();
---
+++ html-js/detect-in-viewport/main.ts
@@ -0,0 +1,40 @@
+
+(function () {
+    function debounce<T extends (...args: any[]) => void>(fn: T, delay: number) {
+        let timerId: ReturnType<typeof setTimeout>;
+        return function (this: any, ...args: Parameters<T>) {
+            clearTimeout(timerId);
+            timerId = setTimeout(() => fn.apply(this, args), delay);
+        };
+    }
+    function createBlocks() {
+        const wrapper = document.querySelector(".wrapper") as HTMLDivElement;
+        const blocks = Array.from({ length: 64 }, (_, i) => {
+            const block = document.createElement("div");
+            block.className = "block";
+            block.textContent = `${i}`;
+            return block;
+        });
+        wrapper.append(...blocks);
+    }
+    createBlocks();
+    function isInViewPort(block: HTMLDivElement) {
+        const rect = block?.getBoundingClientRect();
+        return rect?.top >= 0 && rect?.left >= 0 && rect.bottom <= document.documentElement.clientHeight && rect.right <= document.documentElement.clientWidth
+    }
+
+    function detect() {
+        const results: string[] = [];
+        document.querySelectorAll<HTMLDivElement>(".block")
+            .forEach(block => {
+                if (isInViewPort(block)) {
+                    results.push(block.textContent || "");
+                }
+            });
+        console.log("Items In View: ", results);
+    }
+
+    const debounceScroll = debounce(detect, 500);
+
+    window.addEventListener("scroll", debounceScroll, false);
+})()
